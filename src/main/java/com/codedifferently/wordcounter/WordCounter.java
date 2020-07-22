0cc17658d06b188edef6b3831641f0fa7726b9c1
Chastin Taylor <chastintaylor@gmail.com>
2020-07-22 13:50:35 -0400

    Finished assignment

--- src/main/java/com/codedifferently/wordcounter/WordCounter.java
+++ src/main/java/com/codedifferently/wordcounter/WordCounter.java
@@ -1,4 +1,41 @@
 package com.codedifferently.wordcounter;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class WordCounter {
-}
+
+    public WordCounter() {
+
+    }
+
+    public void count(String inputFile) throws FileNotFoundException
+    {
+        Scanner scan = new Scanner(new File(inputFile));
+        HashMap <String,Integer> wordCounter = new HashMap <String,Integer>();
+        while (scan.hasNext())
+        {
+            String word = scan.next();
+            word = word.replaceAll("[^a-zA-Z ]", "");
+            if(wordCounter.containsKey(word))
+            {
+                wordCounter.put(word, wordCounter.get(word)+1);
+            }
+            else
+            {
+                wordCounter.put(word, 1);
+            }
+        }
+
+        
+              for (Map.Entry<String,Integer> print : wordCounter.entrySet())
+              {
+                  System.out.println(print.getKey()+" = "+print.getValue());
+              }
+           }
+        
+    }
+
--- src/test/java/com/codedifferently/wordcounter/WordCounterTest.java
+++ src/test/java/com/codedifferently/wordcounter/WordCounterTest.java
@@ -1,4 +1,12 @@
 package com.codedifferently.wordcounter;
 
+import java.io.FileNotFoundException;
+
 public class WordCounterTest {
+    public static void main(String[] args) throws FileNotFoundException
+    {
+        WordCounter counter = new WordCounter();
+
+        counter.count("TheModernPrometheus.txt");
+    }
 }
